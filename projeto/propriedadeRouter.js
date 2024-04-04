17f850caf06015c13f4fa3f77440aa29d352375c
joaopedrobagli <joaobagli.pedro1506@gmail.com>
2024-04-04 17:46:16 -0300

    ja editando

--- projeto/propriedadeRouter.js
+++ projeto/propriedadeRouter.js
@@ -13,10 +13,16 @@ const db = admin.database();
 app.get('/', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/Propriedade/propriedade.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+            fs.readFile('src/propriedade/propriedade.html', (e, dados) => {
+                let tabela = "";
+                const docPropriedade = db.ref("propriedade");
+                docPropriedade.once("value", function(snapshot){
+                    tabela = criarTabela(snapshot.val());
+                    dados = dados.toString().replace("{tabela}", tabela);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
@@ -69,51 +75,109 @@ app.get('/editar/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
             fs.readFile('src/Propriedade/editar_propriedade.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+                let id = req.params.id;
+                const docPropriedade = db.ref("propriedade/"+id);
+                docPropriedade.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let longitude = snapshot.val().longitude;
+                    let latitude = snapshot.val().latitude;
+                    let cidade = snapshot.val().cidade;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{longitude}", longitude);
+                    dados = dados.toString().replace("{latitude}", latitude);
+                    dados = dados.toString().replace("{cidade}", cidade);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-// Rota da página para editar os dados de um registro de livro
 app.post('/editar', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    let nome = req.body.nome;
+    let longitude = req.body.longitude;
+    let latitude = req.body.latitude;
+    let cidade = req.body.cidade;
+    let docPropriedade = db.ref("propriedade");
+    docPropriedade.child(id).update(
+        {
+            'nome': nome,
+            'longitude': longitude,
+            'latitude' : latitude,
+            'cidade' : cidade
+        }
+    );
+    res.redirect("/propriedade");
 });
 
-// Rota da página para abrir formulário para excluir um registro de um livro
 app.get('/excluir/:id', (req, res) => {
     fs.readFile('src/cabecalho.html', (e, cabecalho) => {
         fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/Propriedade/excluir_propriedade.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
+            fs.readFile('src/propriedade/excluir_propriedade.html', (e, dados) => {
+                let id = req.params.id;
+                const docLivro = db.ref("propriedade/"+id);
+                docPropriedade.once("value", function(snapshot){
+                    let nome = snapshot.val().nome;
+                    let longitude = snapshot.val().longitude;
+                    let latitude = snapshot.val().latitude;
+                    let cidade = snapshot.val().cidade;
+                    dados = dados.toString().replace("{nome}", nome);
+                    dados = dados.toString().replace("{longitude}", longitude);
+                    dados = dados.toString().replace("{latitude}", latitude);
+                    dados = dados.toString().replace("{cidade}", cidade);
+                    dados = dados.toString().replace("{id}", id);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(cabecalho + dados + rodape);
+                    res.end();
+                });
             });
         });
     });
 });
 
-// Rota da página para excluir um registro de um livro
 app.post('/excluir', urlencodedParser, (req, res) => {
-    fs.readFile('src/cabecalho.html', (e, cabecalho) => {
-        fs.readFile('src/rodape.html', (e, rodape) => {
-            fs.readFile('src/index.html', (e, dados) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(cabecalho + dados + rodape);
-                res.end();
-            });
-        });
-    });
+    let id = req.body.id;
+    const docPropriedade = db.ref("propriedade/"+id);
+    docPropriedade.remove();
+    res.redirect("/propriedade");
 });
+
+function criarTabela(dados) {
+    let tabela = `<table class="table table-striped zebrado">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome da propriedade</th>
+                            <th>longitude</th>
+                            <th>latitude</th>
+                            <th>cidade</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    for (let chave in dados) {
+        tabela += `<tr>
+                        <td>${chave}</td>
+                        <td>${dados[chave].nome}</td>
+                        <td>${dados[chave].longitude}</td>
+                        <td>${dados[chave].latitude}</td>
+                        <td>${dados[chave].cidade}</td>
+                        <td>
+                            <a class="btn btn-outline-warning" href="/propriedade/editar/${chave}">Alterar</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="/propriedade/excluir/${chave}">Excluir</a>
+                        </td>
+                    </tr>`;
+    }            
+    tabela += `</tbody >
+            </table > `;
+    return tabela;
+}
 
 module.exports = app;
